59222aa198aa45ddf37f949755593724cfd60ad8
d74g0n (dragon) <d74g0n@gmail.com>
2019-05-05 23:26:51 -0400

    derp
    derp

--- my_time.js
+++ my_time.js
@@ -10,7 +10,7 @@
 
 
 class dragon_time {
-    constructor(bg = 'transparent', borderD = 'black', color = 'black', top = '10px', left = '0px', right = '0px', w = '64px', h = '19px') {
+    constructor(bg = 'transparent', borderD = 'black', color = 'black', top = '10px', left = '0px', right = '0px', w = '66px', h = '26px') {
         console.log('[dragon_time][created]');
         // init:
         this.top = top;
@@ -47,79 +47,103 @@ class dragon_time {
         this.div.style.marginRight = 'auto';
 
         this.div.onclick = function () {
-            if (dt.logicToggleB) {
-//                 dt.sskDiv('transform','rotateY(0deg)');
+            if (dt.logicToggleA) {
+                //                 dt.sskDiv('transform','rotateY(0deg)');
+
+                dt.sskDiv('height', '66px');
+                dt.sskDiv('borderRadius', '5px');
+                dt.sskDiv('borderTopLeftRadius', '50px');
+                dt.sskDiv('borderTopRightRadius', '50px');
+                dt.sskSpan('background', 'white');
             } else {
-//                dt.sskDiv('transform','rotateY(60deg)');
+                //                dt.sskDiv('transform','rotateY(60deg)');
+                dt.sskDiv('height', '26px');
+                dt.sskDiv('width', '100px');
+                dt.sskDiv('borderRadius', '100px');
+
             }
-            dt.logicToggleB = !dt.logicToggleB;
+            dt.logicToggleA = !dt.logicToggleA;
         }
 
 
         /* -=[ SPAN */
-//        this.span = document.createElement('span');
+        //        this.span = document.createElement('span');
         this.span = document.createElement('span');
         this.span.id = 'time_span';
 
-        this.span.onclick = function () {
-            console.log('[onclick][time_span]');
-            if (dt.logicToggleA) {
-                /*
-                dt.sskDiv('height', '64px');
-                dt.sskDiv('borderRadius','5px');
-                dt.sskDiv('borderTopLeftRadius','50px');
-                dt.sskDiv('borderTopRightRadius','50px');
-                dt.sskSpan('background', 'white');
-                dt.sskSpan('boxShadow', '0px 1px 2px #000');
-                */
-//                dt.sskDiv('transform','rotateX(180deg)');
-                
-            } else {
-                /*
-                dt.sskSpan('background', 'transparent');
-                dt.sskSpan('color', 'white');
-                dt.sskDiv('height', '19px');
-                dt.sskDiv('borderRadius','100px');
-                
-                setTimeout(function(){
-                dt.sskSpan('boxShadow', '0px 0px 0px #000');
-                    }, 999);
-                */
-//                dt.sskDiv('transform','rotateX(360deg)');
-            }
-            dt.logicToggleA = !dt.logicToggleA;
-            
-            
-        }
 
         this.div.appendChild(this.span);
 
+        this.sstyler = document.getElementById('time_span').style;
+
+        this.sstyler.display = 'inherit';
+        this.sstyler.height = '400px';
+
+
+
 
         // SPAN Styling:
-        const ss = this.span.style;
-        this.span.style.display = 'inherit';
-//        this.span.style.display = 'absolute';
-        this.span.style.margin = '0';
-        this.span.style.padding = '0';
-        //        ss.color = this.color;
-        this.span.style.color = this.color;
-        // centering:
-        ss.marginLeft = 'auto';
-        ss.marginRight = 'auto';
-        ss.textAlign = 'center';
+
+        //        this.span.style.display = 'inherit';
+        //        this.span.style.display = 'fixed';
+        //        this.span.style.display = 'fixed';
+        //        
+        //        this.span.style.width = '200px';
+        //        this.span.style.left = 0;
+        /*        this.span.style.right = '100px';
+                this.span.style.width = this.w;
+                this.span.style.height = this.h-4;
+                */
+
+        /*        
+                this.span.style.margin = '0';
+                this.span.style.padding = '4px';
+                //        ss.color = this.color;
+                this.span.style.color = this.color;
+                // centering:
+                this.span.style.marginLeft = 'auto';
+                this.span.style.marginRight = 'auto';
+                this.span.style.margin = 'auto';
+                this.span.style.padding = 'auto';
+                this.span.style.textAlign = 'center';
+                */
+        //        this.span.style.height = '22px';
+
+
         // border:
         this.span.style.border = '2px solid grey';
         this.span.style.borderRadius = '15px';
-//        this.span.style.boxShadow = '0px 0px 2px red';
+        //        this.span.style.boxShadow = '0px 0px 2px red';
         // selection:
         this.span.style.userSelect = 'none';
         this.span.style.fontFamily = 'monospace';
         // EXPERIMENTAL:::
         this.span.style.textShadow = '0px 0px 1px black, 0 0 1px black, 0 0 0.1em black';
 
+
+
+
+        this.span.onclick = function () {
+            console.log('[onclick][time_span]');
+            if (dt.logicToggleB) {} else {}
+            dt.logicToggleB = !dt.logicToggleB;
+
+
+        }
+
+
+
         this.getTime = function () {
             let now = new Date();
-            let time = now.getHours() + ":" + (now.getMinutes() < 10 ? '0' : '') + now.getMinutes() + ":" + (now.getSeconds() < 10 ? '0' : '') + now.getSeconds();
+            //            let time = now.getHours() + ":" + (now.getMinutes() < 10 ? '0' : '') + now.getMinutes() + ":" + (now.getSeconds() < 10 ? '0' : '') + now.getSeconds();
+            //            let time = now.getHours() + ":" + (now.getMinutes() < 10 ? '0' : '') + now.getMinutes();
+
+            let hours = now.getHours();
+            if (hours > 12) {
+                hours -= 12;
+            }
+
+            let time = hours + ":" + (now.getMinutes() < 10 ? '0' : '') + now.getMinutes() + (now.getHours() < 12 ? 'AM' : 'PM');
             document.getElementById('time_span').textContent = time;
         }
 
